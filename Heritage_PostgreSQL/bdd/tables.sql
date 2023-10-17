fcb590a2d3af1bcfd3800f767b249e36d3cdbf78
ScrimaliAnthony <anthony.scrimali@gmail.com>
2023-10-17 10:28:10 +0200

    Ajout de la base de donnée

--- Heritage_PostgreSQL/bdd/tables.sql
+++ Heritage_PostgreSQL/bdd/tables.sql
@@ -1,12 +1,24 @@
 -- Création de la TABLE parent
 DROP TABLE IF EXISTS utilisateur CASCADE;
 CREATE TABLE IF NOT EXISTS utilisateur(
-    id SERIAL PRIMARY KEY,
+    id_utilisateur SERIAL PRIMARY KEY,
     nom VARCHAR(20) NOT NULL,
     prenom VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    email VARCHAR(50) NOT NULL,
+    mdp VARCHAR(255) NOT NULL
 );
 
 -- Création de la TABLE enfant
-DROP TABLE IF EXISTS admin;
-CREATE TABLE IF NOT EXISTS admin() INHERITS (utilisateur);
+DROP TABLE IF EXISTS administrateur;
+CREATE TABLE IF NOT EXISTS administrateur() INHERITS (utilisateur);
+
+DROP TABLE IF EXISTS vendeur;
+CREATE TABLE IF NOT EXISTS vendeur(
+    pays VARCHAR(255),
+    tel INT NOT NULL
+) INHERITS (utilisateur);
+
+DROP TABLE IF EXISTS acheteur;
+CREATE TABLE IF NOT EXISTS acheteur(
+    adresse VARCHAR(255) NOT NULL
+) INHERITS (utilisateur);
